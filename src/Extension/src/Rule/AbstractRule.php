6efccfb5e7bf67758b4f15df9edfb307f928fb38
Denys <denys@joomla-ua.org>
2026-05-14 10:46:36 +0300

    Code refactoring

--- src/Extension/src/Rule/AbstractRule.php
+++ src/Extension/src/Rule/AbstractRule.php
@@ -11,82 +11,83 @@ namespace JUTypography\Rule;
 
 abstract class AbstractRule
 {
-	/**
-	 * @var string
-	 */
-	public $name = 'Name rule';
+    /**
+     * @var string
+     */
+    public $name = 'Name rule';
 
-	/**
-	 * @var mixed[]
-	 */
-	protected $char = [
-		'allQuote' => '«‹»›„“‟”"\'',
-		'allDash'  => '-|‒|–|—',
-		'charEnd'  => '.,!?:;',
-		'char'     => 'а-яєїґa-z',
-		'charCase' => 'а-яєїґa-zA-ЯЄЇҐA-Z',
-		'nbsp'     => '&nbsp;',
-		'dash'     => '&mdash;',
-	];
+    /**
+     * @var array
+     */
+    protected $char = [
+        'allQuote' => '«‹»›„“‟”"\'',
+        'allDash'  => '-|‒|–|—',
+        'charEnd'  => '.,!?:;',
+        'char'     => 'а-яєїґa-z',
+        'charCase' => 'а-яєїґa-zA-ЯЄЇҐA-Z',
+        'nbsp'     => '&nbsp;',
+        'dash'     => '&mdash;',
+    ];
 
-	/**
-	 * @var bool
-	 */
-	protected $active = true;
+    /**
+     * @var bool
+     */
+    protected $active = true;
 
-	/**
-	 * @var int
-	 */
-	protected $sort = 500;
+    /**
+     * @var int
+     */
+    protected $sort = 500;
 
-	/**
-	 * @var mixed[]
-	 */
-	protected $settings = [];
+    /**
+     * @var array
+     */
+    protected $settings = [];
 
-	public function setSort(int $sort): void
-	{
-		$this->sort = $sort;
-	}
+    public function setSort(int $sort): void
+    {
+        $this->sort = $sort;
+    }
 
-	public function getSort(): int
-	{
-		return $this->sort;
-	}
+    public function getSort(): int
+    {
+        return $this->sort;
+    }
 
-	public function setActive(bool $active): void
-	{
-		$this->active = $active;
-	}
+    public function setActive(bool $active): void
+    {
+        $this->active = $active;
+    }
 
-	public function getActive(): bool
-	{
-		return $this->active;
-	}
+    public function getActive(): bool
+    {
+        return $this->active;
+    }
 
-	/**
-	 * @param mixed[] $settings
-	 */
-	public function setSettings(array $settings): void
-	{
-		$this->settings = array_merge($this->settings, $settings);
-	}
+    /**
+     * @param   array  $settings
+     */
+    public function setSettings(array $settings): void
+    {
+        $this->settings = array_merge($this->settings, $settings);
+    }
 
-	/**
-	 * @param mixed $value
-	 */
-	public function setSetting(string $key, $value): void
-	{
-		$this->settings[ $key ] = $value;
-	}
+    /**
+     * @param   string  $key
+     * @param   mixed   $value
+     */
+    public function setSetting(string $key, mixed $value): void
+    {
+        $this->settings[ $key ] = $value;
+    }
 
-	/**
-	 * @return mixed[]
-	 */
-	public function getSettings(): array
-	{
-		return $this->settings;
-	}
+    /**
+     * @return array
+     */
+    public function getSettings(): array
+    {
+        return $this->settings;
+    }
 
-	abstract public function handler(string $text): string;
+    abstract public function handler(string $text): string;
 }
--- src/Extension/src/Rule/Number/DimensionSup.php
+++ src/Extension/src/Rule/Number/DimensionSup.php
@@ -13,34 +13,38 @@ use JUTypography\Rule\AbstractRule;
 
 class DimensionSup extends AbstractRule
 {
-	public $name = 'Dimension Sup';
+    public $name = 'Dimension Sup';
 
-	public function handler(string $text): string
-	{
-		$superscriptMap = [
-			'0' => '⁰',
-			'1' => '¹',
-			'2' => '²',
-			'3' => '³',
-			'4' => '⁴',
-			'5' => '⁵',
-			'6' => '⁶',
-			'7' => '⁷',
-			'8' => '⁸',
-			'9' => '⁹',
-		];
+    public function handler(string $text): string
+    {
+        $superscriptMap = [
+            '0' => '⁰',
+            '1' => '¹',
+            '2' => '²',
+            '3' => '³',
+            '4' => '⁴',
+            '5' => '⁵',
+            '6' => '⁶',
+            '7' => '⁷',
+            '8' => '⁸',
+            '9' => '⁹',
+        ];
 
-		$pattern = '#(м|мм|см|дм|км|гм|m|km|dm|cm|mm)([\d]{1,3})([^' . $this->char[ 'char' ] . '0-9]|$)#iu';
+        $pattern = '#(м|мм|см|дм|км|гм|m|km|dm|cm|mm)([\d]{1,3})([^'.$this->char[ 'char' ].'0-9]|$)#iu';
 
-		return preg_replace_callback($pattern, function ($matches) use ($superscriptMap)
-		{
-			$unit  = $matches[ 1 ];
-			$power = $matches[ 2 ];
-			$after = $matches[ 3 ];
+        return preg_replace_callback(
+            $pattern,
+            static function ($matches) use ($superscriptMap)
+            {
+                $unit = $matches[ 1 ];
+                $power = $matches[ 2 ];
+                $after = $matches[ 3 ];
 
-			$superPower = strtr($power, $superscriptMap);
+                $superPower = strtr($power, $superscriptMap);
 
-			return $unit . $superPower . $after;
-		}, $text);
-	}
+                return $unit.$superPower.$after;
+            },
+            $text
+        );
+    }
 }

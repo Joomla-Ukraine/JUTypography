a6cdcd384f406d9980bf50a7f5e653b17b9732cf
Denys <denys@joomla-ua.org>
2026-06-20 03:33:23 +0300

    Fix Paragraph

--- src/Extension/src/Rule/Html/Paragraph.php
+++ src/Extension/src/Rule/Html/Paragraph.php
@@ -13,48 +13,43 @@ use JUTypography\Rule\AbstractRule;
 
 class Paragraph extends AbstractRule
 {
-	public $name = 'Paragraph <p>';
+    public $name = 'Paragraph <p>';
 
-	protected $active = false;
+    protected $active = false;
 
-	protected $sort = 800;
+    protected $sort = 800;
 
-	public function handler(string $text): string
-	{
-		$text  = trim($text);
-		$first = mb_strpos($text, '<p>');
-		$last  = mb_strrpos($text, '</p>');
+    public function handler(string $text): string
+    {
+        $text = trim($text);
+        $first = mb_strpos($text, '<p>');
+        $last = mb_strrpos($text, '</p>');
 
-		if(false !== $first && false !== $last)
-		{
-			$newText = '';
-			$start   = trim(mb_substr($text, 0, $first));
+        if (false !== $first && false !== $last) {
+            $newText = '';
+            $start = trim(mb_substr($text, 0, $first));
 
-			if(!empty($start))
-			{
-				$newText .= $this->addParagraph($start);
-			}
+            if (!empty($start)) {
+                $newText .= $this->addParagraph($start);
+            }
 
-			$middle  = mb_substr($text, $first + mb_strlen('<p>'), $last - ($first + mb_strlen('<p>')));
-			$newText .= '<p>' . $middle . '</p>';
+            $middle = mb_substr($text, $first + mb_strlen('<p>'), $last - ($first + mb_strlen('<p>')));
+            $newText .= '<p>'.$middle.'</p>';
 
-			$end = trim(mb_substr($text, $last + mb_strlen('</p>')));
+            $end = trim(mb_substr($text, $last + mb_strlen('</p>')));
 
-			if(!empty($end))
-			{
-				$newText .= $this->addParagraph($end);
-			}
+            if (!empty($end)) {
+                $newText .= $this->addParagraph($end);
+            }
 
-			return $newText;
-		}
+            return $newText;
+        }
 
-		return $this->addParagraph($text);
-	}
+        return $this->addParagraph($text);
+    }
 
-	protected function addParagraph(string $text): string
-	{
-		$text = trim($text);
-
-		return '<p>' . $text . '</p>';
-	}
+    protected function addParagraph(string $text): string
+    {
+        return trim($text);
+    }
 }
